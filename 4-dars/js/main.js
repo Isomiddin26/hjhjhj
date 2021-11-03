0303f75dc28d08eb4e43ed76a56bd1fd541e5478
Isomiddin26 <26disel@gmail.com>
2021-11-03 14:43:26 +0500

    4-dars

---
+++ 4-dars/js/main.js
@@ -0,0 +1,45 @@
+var userName = prompt("Ismingiz");
+
+var userSurename = prompt("Famaliyangiz");
+
+var userAge = prompt("Yoshingiz");
+
+var userBorn = prompt("Tug'ilgan joyingiz");
+
+var userLive = prompt("Hozirda yashayotgan joyingiz");
+
+var userWorkOrStudy = prompt("Ishlaysizmi yoki O'qiysizmi?");
+
+var userJob = prompt("Yoqtirgan ishingiz");
+
+var userSubject = prompt("Yoqtirgan faningiz");
+
+var userPlace = prompt("Yoqtirgan joyingiz");
+
+var userLove = prompt("Kimni engkop yahshikorasiz");
+
+console.log(
+    "Ismingiz:" + " " + userName + " " + "\n" +
+    "Famaliyangiz:" + " " + userSurename + " " + "\n" +
+    "Yoshingiz:" + " " + userAge + " " + "\n" +
+    "Tug'ilgan joyingiz:" + " " + userBorn + " " + "\n" +
+    "Hozirda yashayotgan joyingiz:" + " " + userLive + " " + "\n" +
+    "Ishlaysizmi yoki O'qiysizmi?:" + " " + userWorkOrStudy + " " + "\n" +
+    "Yoqtirgan ishingiz:" + " " + userJob + " " + "\n" +
+    "Yoqtirgan faningiz:" + " " + userSubject + " " + "\n" +
+    "Yoqtirgan joyingiz:" + " " + userPlace + " " + "\n" +
+    "Kimni engkop yahshikorasiz:" + " " + userLove + " " + "\n"
+)
+
+alert(
+    "Ismingiz:" + " " + userName + " " + "\n" +
+    "Famaliyangiz:" + " " + userSurename + " " + "\n" +
+    "Yoshingiz:" + " " + userAge + " " + "\n" +
+    "Tug'ilgan joyingiz:" + " " + userBorn + " " + "\n" +
+    "Hozirda yashayotgan joyingiz:" + " " + userLive + " " + "\n" +
+    "Ishlaysizmi yoki O'qiysizmi?:" + " " + userWorkOrStudy + " " + "\n" +
+    "Yoqtirgan ishingiz:" + " " + userJob + " " + "\n" +
+    "Yoqtirgan faningiz:" + " " + userSubject + " " + "\n" +
+    "Yoqtirgan joyingiz:" + " " + userPlace + " " + "\n" +
+    "Kimni engkop yahshikorasiz:" + " " + userLove + " " + "\n"
+)
